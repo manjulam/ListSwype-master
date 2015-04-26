40b9df2a592be576bb5acc0bfe583cda3959beee
AchuthanKrishna <krishna.achuthan@gmail.com>
2015-04-25 22:25:00 -0400

    Changes to user controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -21,119 +21,41 @@ namespace ListSwype.Controllers
 
        public UserController()
         {
-
             this._listRepository = new ListSwypeRepository();
         }
-
-
-        // GET: api/User
-        public IQueryable<user> Getusers()
-        {
-            return db.users;
-        }
-
-        // GET: api/User/5
-        //[ResponseType(typeof(user))]
-        //public IHttpActionResult Getuser(int id)
-        //{
-        //    user user = db.users.Find(id);
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return Ok(user);
-        //}
 
         // GET: api/User/5
         public UserDTO GetUser(int id)
         {
-           return _listRepository.GetUserbyId(id);
-            
+            return _listRepository.GetUserbyId(id);
         }
-        // PUT: api/User/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult Putuser(int id, user user)
+
+        // GET: api/User/getuserbyemail/name@example.com/
+        [HttpGet]
+        [ActionName("GetUserByEmail")]
+        public UserDTO GetUserByEmail(string  email)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != user.ID)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(user).State = EntityState.Modified;
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!userExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
+            return _listRepository.GetUserbyEmail(email);
         }
 
-        //// POST: api/User
-        //[ResponseType(typeof(user))]
-        //public IHttpActionResult Postuser(user user)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
-
-        //    db.users.Add(user);
-        //    db.SaveChanges();
-
-        //    return CreatedAtRoute("DefaultApi", new { id = user.ID }, user);
-        //}
-
-        
-        public void Post([FromBody]UserDTO user)
+        // POST: api/User
+        public string Post([FromBody]UserDTO user)
         {
-            _listRepository.SaveUser(user);
+            return _listRepository.SaveUser(user);
         }
+
         // DELETE: api/User/5
-        [ResponseType(typeof(user))]
-        public IHttpActionResult Deleteuser(int id)
+        public bool DeleteUser(int id)
         {
-            user user = db.users.Find(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            db.users.Remove(user);
-            db.SaveChanges();
-
-            return Ok(user);
+            return _listRepository.DeleteUserByID(id);
         }
 
-        protected override void Dispose(bool disposing)
+        // DELETE: api/User/deleteuserbyemail/name@example.com/
+        [HttpDelete]
+        [ActionName("DeleteUserByEmail")]
+        public bool DeleteUserByEmail(string email)
         {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        private bool userExists(int id)
-        {
-            return db.users.Count(e => e.ID == id) > 0;
+            return _listRepository.DeleteUserByEmail(email);
         }
     }
 }
